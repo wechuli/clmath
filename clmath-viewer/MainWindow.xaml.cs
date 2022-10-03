b10933e012c5ffda74d2ddc41e0850106b010722
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-10-03 15:10:18 +0200

    add viewer project

---
+++ clmath-viewer/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using SharpGL.WPF;
+
+namespace clmath.viewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void Initialize(object sender, OpenGLRoutedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Draw(object sender, OpenGLRoutedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MouseHandler(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ClickHandler(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
