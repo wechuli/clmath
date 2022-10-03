17b83f783bb2ec9256c30d4f7f8d5d8dfc96f490
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-10-03 15:00:32 +0200

    fuck cmake
    remove idea

--- src/AbstractVisitor.h
+++
@@ -1,7 +0,0 @@
-#include "MathBaseVisitor.h"
-
-class AbstractVisitor : public MathBaseVisitor {
-    bool shouldVisitNextChild(antlr4::tree::ParseTree *tree, const std::any &any) override {
-        return any.has_value();
-    }
-};
--- src/MathContext.cpp
+++
@@ -1,1 +0,0 @@
-#include "MathContext.h"
