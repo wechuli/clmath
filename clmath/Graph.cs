df8d6cdad306c3cb93844ffe37a14d48d906800a
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-10-08 16:12:09 +0200

    Update Graph.cs

--- clmath/Graph.cs
+++ clmath/Graph.cs
@@ -120,7 +120,7 @@ public sealed class GraphWindow : IDisposable
         gl.UseProgram(shaders);
         gl.BindVertexArray(ax_vao);
         gl.DrawArrays(PrimitiveType.Lines, 0, 4);
-        //gl.DrawElements(PrimitiveType.Lines, 4, DrawElementsType.UnsignedInt, 0);
+        //gl.DrawElements(PrimitiveType.Lines, 2, DrawElementsType.UnsignedInt, 0);
         
         gl.Flush();
     }
