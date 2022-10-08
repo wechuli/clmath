3a02f721ccf5eed3dcfe25cb27015cfa1e27c8f7
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-10-08 17:35:00 +0200

    we have a sine

--- clmath/Graph.cs
+++ clmath/Graph.cs
@@ -1,6 +1,6 @@
 ﻿using System.Drawing;
+using System.Numerics;
 using System.Runtime.InteropServices;
-using GlmSharp;
 using Silk.NET.Maths;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
@@ -11,8 +11,8 @@ public sealed class GraphWindow : IDisposable
 {
     private static readonly DirectoryInfo AssemblyDir;
     private readonly MathContext ctx;
-    private int scaleX = 1;
-    private int scaleY = 1;
+    private double scaleX = 3.5;
+    private double scaleY = 1.5;
     private Component x;
 
     private readonly Component fx;
@@ -67,6 +67,10 @@ public sealed class GraphWindow : IDisposable
     private uint ax_vao;
     private uint ax_vbo;
     //private uint ax_ebo;
+    private uint cv_vao;
+    private uint cv_vbo;
+    //private uint cv_ebo;
+    private uint cv_count;
     private uint shaders;
 
     private void Load()
@@ -134,15 +138,54 @@ public sealed class GraphWindow : IDisposable
         gl.Flush();
     }
 
-    private unsafe void InitGraphCurve() {}
+    private unsafe void InitGraphCurve()
+    {
+        double step = 0.1;//Math.Sqrt(scaleX) * 2.5;
+        List<Vector2D<double>> curve = new();
+        var lim = scaleX + 1;
+        for (x.arg = -scaleX; (double)x.arg < lim; x.arg = (double)x.arg + step)
+        {
+            var y = fx.Evaluate(ctx);
+            curve.Add(new Vector2D<double>((double)x.arg, y));
+        }
+        var curve_verts = curve.SelectMany(v => new[] { v.X / scaleX, v.Y / scaleY }).ToArray();
+        cv_count = Convert.ToUInt32(curve_verts.Length);
+
+        cv_vao = gl.GenVertexArray();
+        cv_vbo = gl.GenBuffer();
+        //cv_ebo = gl.GenBuffer();
+        gl.BindVertexArray(cv_vao);
+
+        gl.BindBuffer(BufferTargetARB.ArrayBuffer, cv_vbo);
+        fixed (double* cv_vtx_ptr = &curve_verts[0])
+            gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(curve_verts.Length * sizeof(double)), cv_vtx_ptr,
+                GLEnum.StaticDraw);
+
+        //gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, cv_ebo);
+        //fixed(uint* cv_idx_ptr = &curve_indices[0])
+        //    gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(curve _indices.Length * sizeof(uint)), cv_idx_ptr, GLEnum.StaticDraw);
+
+        gl.VertexAttribPointer(0, 2, VertexAttribPointerType.Double, false, 0, null);
+        gl.EnableVertexAttribArray(0);
+
+        gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+        //gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
+        gl.BindVertexArray(0);
+        gl.Flush();
+    }
 
     private void Render(double obj)
     {
         gl.Clear(16640U); // color & depth buffer
         
         gl.UseProgram(shaders);
+        
         gl.BindVertexArray(ax_vao);
         gl.DrawArrays(PrimitiveType.Lines, 0, 4);
+        //gl.DrawElements(PrimitiveType.Lines, 2, DrawElementsType.UnsignedInt, 0);
+        
+        gl.BindVertexArray(cv_vao);
+        gl.DrawArrays(PrimitiveType.LineStrip, 0, cv_count / 2);
         //gl.DrawElements(PrimitiveType.Lines, 2, DrawElementsType.UnsignedInt, 0);
         
         gl.Flush();
